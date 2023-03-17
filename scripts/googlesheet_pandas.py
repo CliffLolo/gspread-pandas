198a9000f1b326019c48bd237f079b505f04a407
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-03-17 17:36:39 +0000

    added most popular city insights and visual

--- scripts/googlesheet_pandas.py
+++ scripts/googlesheet_pandas.py
@@ -26,7 +26,20 @@ print(f"Best Selling Product: {best_selling_product}")
 
 # Visualize Best Selling Product
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Product", y="Quantity Ordered", data=df, order=df.groupby('Product')['Quantity Ordered'].sum().sort_values(ascending=False).index)
+sns.barplot(x="Product", y="Quantity Ordered", data=df,
+            order=df.groupby('Product')['Quantity Ordered'].sum().sort_values(ascending=False).index)
 plt.xticks(rotation=90)
 plt.title('Best Selling Product')
+plt.show()
+
+# Insight 3: Most Popular City
+df['City'] = df['Purchase Address'].apply(lambda x: x.split(',')[1])
+most_popular_city = df.groupby('City')['Order ID'].count().idxmax()
+print(f"Most Popular City: {most_popular_city}")
+
+# Visualize Most Popular City
+plt.figure(figsize=(10, 6))
+sns.countplot(x='City', data=df, order=df['City'].value_counts().index)
+plt.xticks(rotation=90)
+plt.title('Most Popular City')
 plt.show()
