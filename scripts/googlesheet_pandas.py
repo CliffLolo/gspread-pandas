46e44a29ef7a46224165ee4a540db766a8e64d16
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-03-17 17:30:44 +0000

    added total revenue insight

--- scripts/googlesheet_pandas.py
+++ scripts/googlesheet_pandas.py
@@ -8,3 +8,13 @@ sheet = goocleclient.open("Product Data").worksheet("January")
 
 # Load data into a pandas DataFrame
 df = pd.DataFrame(sheet.get_all_records())
+
+# Convert Order Date to datetime
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+
+# Add a Total Sales column
+df['Total Sales'] = df['Quantity Ordered'] * df['Price Each']
+#
+# Insight 1: Total Revenue
+total_revenue = df['Total Sales'].sum()
+print(f"Total Revenue: ${total_revenue:.2f}")
